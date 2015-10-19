a93a8e7a63324930da6c1b4dbc7cae0604cf70ff
ananth039 <ananth039@gmail.com>
2015-10-19 18:27:57 +0530

    master page on quality thought project on asp.net

---
+++ Hostel Managment system/Hostel Managment system/Users Regustration.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace Hostel_Managment_system
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string fname = txtFirstName.Text;
+            string lname = txtLastName.Text;
+            string gender;
+            int id = 1; ;
+            string email = txtEmail.Text;
+            string phone = txtPhoneNumber.Text;
+            string username = txtUserName.Text;
+            string pwd = txtPwd.Text;
+            string cpwd = txtcpwd.Text;
+            if (rbmale.Checked == true)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+            string conectionstring = ConfigurationManager.ConnectionStrings["ananth"].ConnectionString;
+            try
+            {
+
+                using (SqlConnection sc = new SqlConnection(conectionstring))
+                {
+                    sc.Open();
+                    string query = "insert into UsersRegistration(FirstName,LastName,Gender,Email,phoneNumber,username,Password,cpwd) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')";
+                    SqlCommand insertCommand = new SqlCommand(string.Format(query, fname, lname, gender, email, phone, username, pwd, cpwd), sc);
+                    insertCommand.ExecuteNonQuery();
+                }
+            }catch(Exception ex)
+            {
+                lblStatus.Text = ex.Message;
+            }
+
+        }
+
+        protected void btndisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btndisplay_Click1(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ asp.net programs/gridview Reader/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="gridview_Reader.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="sno" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="sno" HeaderText="sno" ReadOnly="True" SortExpression="sno" />
+                <asp:BoundField DataField="Sname" HeaderText="Sname" SortExpression="Sname" />
+                <asp:BoundField DataField="fname" HeaderText="fname" SortExpression="fname" />
+                <asp:BoundField DataField="phonenumber" HeaderText="phonenumber" SortExpression="phonenumber" />
+                <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" />
+            </Columns>
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:Anantha KumarConnectionString %>" SelectCommand="SELECT * FROM [student]"></asp:SqlDataSource>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
