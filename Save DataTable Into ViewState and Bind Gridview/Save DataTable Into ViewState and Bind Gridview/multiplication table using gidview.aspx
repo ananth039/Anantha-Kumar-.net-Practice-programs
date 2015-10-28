d7c62576ee997ee2d013ab7863494909257d8e3c
ananth039 <ananth039@gmail.com>
2015-10-28 21:44:59 +0530

    multiplication table using gridview

---
+++ Save DataTable Into ViewState and Bind Gridview/Save DataTable Into ViewState and Bind Gridview/multiplication table using gidview.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="multiplication table using gidview.aspx.cs" Inherits="Save_DataTable_Into_ViewState_and_Bind_Gridview.multiplication_table_using_gidview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+             <h1 align="center">Multiplication</h1>
+    <div align="center">
+        Enter Number&nbsp;&nbsp;<asp:TextBox ID="TextBox1" runat="server"/><br /><br />
+Enter Range&nbsp;&nbsp;<asp:TextBox ID="TextBox2" runat="server"/><br /><br />
+<asp:Button ID="Button1" runat="server" Text="Product table" OnClick="Button1_Click" />
+        <br />  <br />
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    
+    </form>
+</body>
+</html>
---
+++ Save DataTable Into ViewState and Bind Gridview/Save DataTable Into ViewState and Bind Gridview/multiplication table using gidview.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Save_DataTable_Into_ViewState_and_Bind_Gridview
+{
+    public partial class multiplication_table_using_gidview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(TextBox1.Text);
+            int range = int.Parse(TextBox2.Text);
+            DataTable table = new DataTable();
+            DataRow dr;
+            table.TableName = "Multiplication table";
+           
+            table.Columns.Add(new DataColumn("Multiply", typeof(int)));
+            table.Columns.Add(new DataColumn("Number", typeof(int)));
+            table.Columns.Add(new DataColumn("Result", typeof(int)));
+            for(int i=1;i<=range;i++)
+            {
+                dr=table.NewRow();
+                dr["Multiply"] = i;
+                dr["number"] = number; 
+                dr["Result"] = i * number;
+                table.Rows.Add(dr);
+            }
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+        }
+    }
+}
