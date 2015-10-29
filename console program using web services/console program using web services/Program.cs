32e9d7f3f9d7007f5cfabe4c50e71111076c8af7
ananth039 <ananth039@gmail.com>
2015-10-29 17:30:03 +0530

    webservices using console program

---
+++ console program using web services/console program using web services/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_program_using_web_services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService.Calculator cal = new CalculatorService.Calculator();
+            Console.WriteLine("Add:"+ cal.Add(2, 5));
+
+            Console.WriteLine("Subtract:" + cal.Subtract(100, 85));
+            Console.WriteLine("Multiply:" + cal.Multiply(200, 35));
+            Console.WriteLine("Divide:" + cal.Divide(500, 200));
+
+
+
+        }
+    }
+}
