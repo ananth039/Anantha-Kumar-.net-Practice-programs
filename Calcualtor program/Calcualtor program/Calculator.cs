f743dedd18a1570543c4f053be25ab14884058eb
ananth039 <ananth039@gmail.com>
2015-10-29 12:55:54 +0530

    Calculator program

---
+++ Calcualtor program/Calcualtor program/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcualtor_program
+{
+    class Calculator
+    {
+       
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtarction(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+        public double Percentage(double a, double b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ Calcualtor program/Calcualtor program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcualtor_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Console.WriteLine("Addition: {0}",cal.Addition(2,3));
+            Console.WriteLine("Subtarction: {0}", cal.Subtarction(10, 5));
+            Console.WriteLine("Division: {0}", cal.Division(25, 5));
+            Console.WriteLine("Multiplication: {0}", cal.Multiplication(100, 25));
+            Console.WriteLine("Percentage: {0}", cal.Percentage(50, 10));
+        }
+    }
+}
